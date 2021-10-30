d3d8745210ad0391b9ecf525277e0311a08743ec
aombk <aombkappa@gmail.com>
2021-10-31 00:44:37 +0300

    some changes

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -28,9 +28,9 @@ void SimpEQAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour (juce::Colours::red);
+    g.setFont (32.0f);
+    g.drawFittedText ("simpEQ", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SimpEQAudioProcessorEditor::resized()
